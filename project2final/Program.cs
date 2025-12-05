6a9fb439dd6b87ba948ee5067ffe64fcac5bec1d
dave744 <davekurdshmeds@gmail.com>
2025-12-05 15:18:50 +0400

    Add files via upload

---
+++ project2final/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using BookLibraryApp;
+
+var manager = new BookManager();
+
+Console.WriteLine("=== Book Library ===");
+
+while (true)
+{
+    Console.WriteLine("\n1. Add a Book");
+    Console.WriteLine("2. View Books");
+    Console.WriteLine("3. Search");
+    Console.WriteLine("4. Quit");
+    Console.Write("Choice: ");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            AddBook();
+            break;
+
+        case "2":
+            manager.ShowAll();
+            break;
+
+        case "3":
+            Console.WriteLine();
+            var q = InputHelper.ReadText("Title keyword: ");
+            manager.Search(q);
+            break;
+
+        case "4":
+            Console.WriteLine("Goodbye!");
+            return;
+
+        default:
+            Console.WriteLine("Invalid option.");
+            break;
+    }
+}
+
+void AddBook()
+{
+    Console.WriteLine("\nAdding a new book...\n");
+    string title = InputHelper.ReadText("Title: ");
+    string author = InputHelper.ReadText("Author: ");
+    int year = InputHelper.ReadYear("Year: ");
+
+    manager.Add(new Book(title, author, year));
+}
